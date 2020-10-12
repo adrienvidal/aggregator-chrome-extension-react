005e46e4a10660e0da25c85dc0b47f927794b83d
Adrien Vidal <adriensg.vidal@gmail.com>
2020-10-12 14:29:02 +0200

    add default image

--- src/pages/Newtab/Views/Home/BookmarksList/BookmarkItem/index.jsx
+++ src/pages/Newtab/Views/Home/BookmarksList/BookmarkItem/index.jsx
@@ -5,13 +5,18 @@ import {
 import './styles.scss'
 
 export default function BookmarkItem({aggregatorBookmark}) {
+	const defaultImage = 'https://images.unsplash.com/photo-1602444307048-952df9146135?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1934&q=80'
+
 	return (
 		<Card
 			className="mb-3"
 			onClick={() => window.open(aggregatorBookmark.link)}
 		>
 			<div className="img-wrapper">
-				<img src={aggregatorBookmark.image} />
+				<img
+					src={aggregatorBookmark.image || defaultImage}
+					alt="img"
+				/>
 			</div>
 			<Card.Body>
 				<Card.Title>{aggregatorBookmark.title}</Card.Title>
